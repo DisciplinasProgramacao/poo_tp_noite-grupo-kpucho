e43076b0175f62ab9a5004f74e60671dcf03ddc0
Henrique Costa Marques <104331565+henriqueCMarques@users.noreply.github.com>
2023-04-19 12:51:27 -0300

    Update Serie.java
    Retirando a técnica Singleton de todas as classes, agora os métodos da classe Aplicativo são estáticos e todos os outros métodos conseguem utiliza-los.

--- codigo/src/Serie.java
+++ codigo/src/Serie.java
@@ -4,7 +4,6 @@ public class Serie
     private String  nome;
     private String idioma; // Falta Implementar a Lista de idiomas / certos idiomas pré-selecionados que podem ser utilizados, conforme a primeira versão do diagrama 
     private String genero; // Falta Implementar a Lista de generos / certos gêneros pré-selecionados que podem utilizadas, conforme a primeira versão do diagrama 
-    private Aplicativo aplicativo ; // Objeto do aplicativo é estático, pois só se tem um aplicativo de streaming
 
     
     private String descricao; // Não implementado
@@ -16,9 +15,9 @@ public class Serie
         this.idioma = idioma;
         this.genero = genero;
 
-        this.aplicativo = Aplicativo.getInstance(); // Instânciando o aplicativo
-        aplicativo.adicionarSerie(this); // Adicionando a série no Hashmap do aplicativo logo após  seu construtor ser chamado
+        Aplicativo.adicionarSerie(this); // Adicionando a série no Hashmap do aplicativo logo após  seu construtor ser chamado
     }
+    
     protected boolean assitirSerie() // Método para registrar a visualização da série
     {
         aumentarContagemVisualizacao(); // Chamando o metodo que irá adicionar visualizações na série assistida
