2ecb9a42c4ef6edce663affc1f0cba5d96c9cf10
Henrique Costa Marques <104331565+henriqueCMarques@users.noreply.github.com>
2023-04-19 13:45:28 -0300

    Update Serie.java
    Atualização para atender requisitos da parte 2 do trabalho

--- codigo/src/Serie.java
+++ codigo/src/Serie.java
@@ -1,7 +1,9 @@
 
 public class Serie
 {
+    private String IdSerie;
     private String  nome;
+    private String dataDeLancamento;
     private String idioma; // Falta Implementar a Lista de idiomas / certos idiomas pré-selecionados que podem ser utilizados, conforme a primeira versão do diagrama 
     private String genero; // Falta Implementar a Lista de generos / certos gêneros pré-selecionados que podem utilizadas, conforme a primeira versão do diagrama 
 
@@ -9,18 +11,20 @@ public class Serie
     private String descricao; // Não implementado
     private int contagemVisualizacao; 
     
-    public Serie(String nome, String idioma, String genero)
+    public Serie(String IdSerie, String nome, String dataDeLancamento,String idioma, String genero)
     {
+        this.IdSerie = IdSerie;
         this.nome = nome;
+        this.dataDeLancamento = dataDeLancamento;
         this.idioma = idioma;
         this.genero = genero;
 
         Aplicativo.adicionarSerie(this); // Adicionando a série no Hashmap do aplicativo logo após  seu construtor ser chamado
     }
-    
+
     protected boolean assitirSerie() // Método para registrar a visualização da série
     {
-        aumentarContagemVisualizacao(); // Chamando o metodo que irá adicionar visualizações na série assistida
+        aumentarContagemVisualizacao();
         return true;
     }
 
