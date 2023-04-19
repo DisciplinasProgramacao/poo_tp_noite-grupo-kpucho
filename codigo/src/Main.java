efd955f1daf3dde76dbfc7a3cbacfd9a311481d8
Henrique Costa Marques <104331565+henriqueCMarques@users.noreply.github.com>
2023-04-19 13:46:00 -0300

    Update Main.java
    Atualização para atender os requisitos da parte 2 do trabalho

--- codigo/src/Main.java
+++ codigo/src/Main.java
@@ -1,42 +1,22 @@
+import java.io.IOException;
+
 public class Main 
 {
     
-public static void main(String[]args)
+public static void main(String[]args) throws IOException
 {
     Aplicativo aplicativo = new Aplicativo();
-    Conta henrique = new Conta("oi", "oi1213", 1547);
-    Serie Matrix = new Serie("Matrix", "PT-bt", "Acao");
-
-    henrique.buscarSerieNoAplicativo("Titanic");
-    henrique.buscarSerieNoAplicativo("Matrix");
-
-    Aplicativo.criarConta("oi", "oi1213", 1111);
-    Aplicativo.buscarSerie("Matrix");
-
-    henrique.assistirSerie("Matrix");
-    henrique.assistirSerie("Matrix");
-    henrique.assistirSerie("Matrix");
-    Serie Titanic = new Serie("Titanic", "PT-BR", "Drama");
+    Conta henrique = new Conta("henrique", "henriquecm122", "1547");
+    Aplicativo.buscarSerie("Titanic");
     henrique.assistirSerie("Titanic");
-    henrique.assistirSerie("Panico");
-    henrique.buscarSerieNoAplicativo("Panico");
-    henrique.buscarSerieNoAplicativo("Titanic");
-    henrique.adicionarSerieEmListaDeAssistirFuturamente("Panico");
-    henrique.adicionarSerieEmListaDeAssistirFuturamente("Titanic");
-    System.out.println(henrique.retornaListaSeriesAssistirFuturamente());
-    System.out.println(henrique.retornaListaSeriesJaAssistidas());
+    henrique.assistirSerie("Titanic");
+    
+    Conta Lucas = aplicativo.getConta("Lucas22");
+    Lucas.assistirSerie("Titanic");
+    Conta testeLucas = new Conta("lucas", "Lucas22", "13342");
+    
 
-    henrique.removerSerieEmListaDeAssistirFuturamente("Titanic");
-    System.out.println(henrique.retornaListaSeriesAssistirFuturamente());
-    Conta lucas = new Conta("Lucas@", "oi1213", 123);
 
-    henrique.adicionarSerieEmListaDeAssistirFuturamente("Titanic");
-    henrique.adicionarSerieEmListaDeAssistirFuturamente("Matrix");
-    henrique.buscarSerieEmSuaListaSeriesAssistirFuturamente("Matrix");
-    henrique.buscarSerieEmSuaListaSeriesAssistirFuturamente("Panico");
-    henrique.buscarSerieEmSuaListaSeriesAssistidas("Panico");
-
-    System.out.println(Matrix.getContagemVisualizacao()); 
    
     
 
