c982476adf049746c2e2fa640227bf839457c407
Henrique Costa Marques <104331565+henriqueCMarques@users.noreply.github.com>
2023-04-19 13:43:19 -0300

    Update Aplicativo.java
    Adicionando a leitura do arquivos Espectadores e SériesArq e criando objetos respectivos dos arquivos.

--- codigo/src/Aplicativo.java
+++ codigo/src/Aplicativo.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class Aplicativo
 {
@@ -6,14 +7,16 @@ public class Aplicativo
     private static HashMap<String, Conta> Contas;
     private static HashMap<String , Serie> Series;
    
-    public Aplicativo() // Construtor privado para impedir a criação de múltiplas instâncias, já que só existe um aplicativo
+    public Aplicativo() throws IOException
     {
         Contas = new HashMap<>();
         Series = new HashMap<>();
+        Series = carregarArquivoSeries(); 
+        Contas = carregarArquivoEspectadores();
    
     }
 
-    public static boolean criarConta(String email, String login, int senha) // Método que irá criar conta do usuário no Aplicativo 
+    public static boolean criarConta(String nome, String login, String senha) // Método que irá criar conta do usuário no Aplicativo 
     {
         if(Contas.containsKey(login)) // Condição para verificar se existe ou não um login associado a uma conta já criada no Aplicativo 
         {
@@ -21,7 +24,7 @@ public class Aplicativo
             return false;
         }
         
-            Conta novaConta = new Conta(login, email, senha); // Cria uma nova conta e adiciona ao Hasmap de contas
+            Conta novaConta = new Conta(nome, login, senha); // Cria uma nova conta e adiciona ao Hasmap de contas
             Contas.put(login, novaConta);
             return true;
         
@@ -40,6 +43,47 @@ public class Aplicativo
          return true;
     }
 
+
+    private static HashMap<String, Serie> carregarArquivoSeries() throws IOException 
+    {                                                                                // Método para carregar Series por meio da leitura do arquivo txt 'SériesArq'
+        BufferedReader br = new BufferedReader(new FileReader(new File ("./arquivos/SériesArq.txt")));
+        String linha ;
+
+        while((linha = br.readLine()) != null)
+        {
+            String IdSerie = linha.split(";")[0];
+            String nomeSerie = linha.split(";")[1];
+            String dataLancamento = linha.split(";")[2];
+
+            Serie serieAdicionada = new Serie(IdSerie, nomeSerie, dataLancamento, "null", "null"  ); // Criando uma nova Serie com base na leitura do arquivo
+
+            Series.put(nomeSerie, serieAdicionada); // Adicionando essa récem criada série ao Hasmap de Serie
+        }
+      
+        return Series;
+    }
+
+
+    private static HashMap<String, Conta> carregarArquivoEspectadores() throws IOException
+    {                                                          // Método para carregar Contas por meio da leitura do arquivo txt 'Espectadores'
+        BufferedReader br = new BufferedReader(new FileReader(new File ("./arquivos/Espectadores.txt")));
+        String linha;
+
+        while((linha = br.readLine()) != null)
+        {
+            String nome = linha.split(";")[0];
+            String login = linha.split(";")[1];
+            String senha = linha.split(";")[2];
+
+            Conta conta = new Conta(nome, login, senha); // Criando uma nova Conta com base na leitura do arquivo
+            Contas.put(login, conta); // Adicionando essa récem criada conta ao Hashmap de Contas
+
+        }
+      
+        return Contas;
+    }
+
+
     public static void adicionarSerie(Serie novaSerie) // Adiciona a série no Hasmap do aplicativo
     {
         Series.put(novaSerie.getNome(), novaSerie);
@@ -49,7 +93,6 @@ public class Aplicativo
     {
         if(Series.containsKey(nomeSerie)) // Verificando no Hasmap se a chave (nome), passado por parametro, do objeto existe  
         {
-            System.out.println("Série existente: "+nomeSerie);
             return true;
         }
         System.out.println("Serie não existe:" +nomeSerie);
@@ -64,6 +107,11 @@ public class Aplicativo
             return serieExistente;
         }
         return null;
+    }
+
+    public Conta getConta(String login)  // Método para acessar o Hashmap de Contas e devolver aquela que possui o login passado por parâmetro. Método criado para acesso as contas no main
+    {
+        return Contas.get(login);
     }
 }
 
