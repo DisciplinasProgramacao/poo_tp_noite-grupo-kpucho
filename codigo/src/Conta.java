8af33385d212ab97f916c45cc17711f6584564d7
Henrique Costa Marques <104331565+henriqueCMarques@users.noreply.github.com>
2023-04-20 10:35:22 -0300

    Update Conta.java
    Colocando o método 'Aplicativo.criarConta(this);' novamente que foi deletado sem querer no último commit antes deste.

--- codigo/src/Conta.java
+++ codigo/src/Conta.java
@@ -18,6 +18,8 @@ public class Conta
         this.nome = nome;
         this.login = login;
         this.senha = senha;
+        Aplicativo.criarConta(this); // Chama o método criarConta do aplicativo, passando o objeto criado dessa classe como parâmetro para ser adicionado ao Hasmap de Contas presente no Aplicativo
+
         
         
         this.ListaSeriesAssistirFuturamente = new ArrayList<>(); // Criando uma lista de series para assistir futuramente. Cada conta vai ter uma lista individual e diferente por isso o uso do .this
