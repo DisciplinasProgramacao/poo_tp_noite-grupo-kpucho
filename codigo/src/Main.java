a039395db9e47f8e46c335826814bbccf44af9f4
Henrique Costa Marques <104331565+henriqueCMarques@users.noreply.github.com>
2023-05-16 12:38:45 -0300

    Update Main.java
    Testes simples para o carregamento de arquivos.

--- codigo/src/Main.java
+++ codigo/src/Main.java
@@ -8,36 +8,23 @@ public class Main
 public static void main(String[]args) throws IOException
 {
     Aplicativo aplicativo = new Aplicativo();
-    Conta henrique = new Conta("henrique", "henriquecm122", "1547");
+    Aplicativo.carregarArquivoEspectadores();
+    Aplicativo.carregarArquivoSeries();
+    Aplicativo.carregarArquivoAudiencia();
 
-    System.out.println(Aplicativo.buscarSeriePorNome("Titanic"));
-    henrique.assistirSerie("Titanic");
-    henrique.assistirSerie("Titanic");
-    
-    Conta Lucas = aplicativo.getConta("Lucas22");
-    Serie titanic = aplicativo.getSerie("Titanic");
-    System.out.println( titanic.getContagemVisualizacao()); 
+    Conta ada = Aplicativo.getConta("Ada3");
 
-    Lucas.assistirSerie("Titanic");
-    Conta testeLucas = new Conta("lucas", "Lucas22", "13342");
+    ada.assistirSerie("Pink is the new Red");
+    ada.assistirSerie("Pink is the new Red");
 
-    System.out.println(Lucas.retornaListaSeriesJaAssistidas()); 
-    
-    System.out.println("=========");
+    System.out.println( ada.retornaListaSeriesJaAssistidas());
+    Serie pink = Aplicativo.getSerie("3481");
+    System.out.println(pink.getContagemVisualizacao());
+  
 
-    System.out.println(titanic);
-
-    titanic.setIdioma("PT-BR");
-    System.out.println(Aplicativo.buscarSeriesPorIdioma("PT-BR"));
-
-    System.out.println(    henrique.buscarSeriesNoAplicativoPorIdioma("PT-BR"));
-
-    System.out.println("============");
-    
-    System.out.println(  Aplicativo.realizarLogin("Lucas22", "123"));
-
-    System.out.println(  Aplicativo.realizarLogin("Lucas22", "1222"));
 }
+
+
 
 
 
