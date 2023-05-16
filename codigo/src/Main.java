6c9b75fe43970ccac2ced44bce57f62e6bfc961a
Henrique Costa Marques <104331565+henriqueCMarques@users.noreply.github.com>
2023-05-16 13:13:47 -0300

    Update Main.java
    Adicionando mais alguns testes simples.

--- codigo/src/Main.java
+++ codigo/src/Main.java
@@ -8,9 +8,6 @@ public class Main
 public static void main(String[]args) throws IOException
 {
     Aplicativo aplicativo = new Aplicativo();
-    Aplicativo.carregarArquivoEspectadores();
-    Aplicativo.carregarArquivoSeries();
-    Aplicativo.carregarArquivoAudiencia();
 
     Conta ada = Aplicativo.getConta("Ada3");
 
@@ -20,8 +17,10 @@ public static void main(String[]args) throws IOException
     System.out.println( ada.retornaListaSeriesJaAssistidas());
     Serie pink = Aplicativo.getSerie("3481");
     System.out.println(pink.getContagemVisualizacao());
-  
-
+    
+    Conta x = Aplicativo.getConta("Mar123542");
+    System.out.println(x.retornaListaSeriesAssistirFuturamente());
+    System.out.println(x.retornaListaSeriesJaAssistidas());
 }
 
 
