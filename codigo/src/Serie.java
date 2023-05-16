9fec21074ae60d492fe49671360660b360b8779d
Henrique Costa Marques <104331565+henriqueCMarques@users.noreply.github.com>
2023-05-16 14:10:54 -0300

    Update Serie.java

--- codigo/src/Serie.java
+++ codigo/src/Serie.java
@@ -1,66 +1,33 @@
 package src;
-public class Serie
+public class Serie extends Midia
 {
-    private String IdSerie;
-    private String nome;
-    private String dataDeLancamento;
-    private String idioma; // Falta Implementar a Lista de idiomas / certos idiomas pré-selecionados que podem ser utilizados, conforme a primeira versão do diagrama 
-    private String genero; // Falta Implementar a Lista de generos / certos gêneros pré-selecionados que podem utilizadas, conforme a primeira versão do diagrama 
-
+    private int quantidadeEpisodios;
     
     private String descricao; // Não implementado
-    private int contagemVisualizacao; 
     
-    public Serie(String IdSerie, String nome, String dataDeLancamento,String idioma, String genero)
+    public Serie(String IdSerie, String nome, String dataDeLancamento,String idioma, String genero, int quantidadeEpisodios, int contagemVisualizacao)
     {
-        this.IdSerie = IdSerie;
-        this.nome = nome;
-        this.dataDeLancamento = dataDeLancamento;
-        this.idioma = idioma;
-        this.genero = genero;
-
-        Aplicativo.adicionarSerie(this); // Adicionando a série no Hashmap do aplicativo logo após  seu construtor ser chamado
+        super(IdSerie, nome, dataDeLancamento, idioma, genero, contagemVisualizacao);
+        this.quantidadeEpisodios = quantidadeEpisodios;
     }
 
+    
     protected boolean assitirSerie() // Método para registrar a visualização da série
     {
         aumentarContagemVisualizacao();
         return true;
     }
 
-    private void aumentarContagemVisualizacao() // Método que aumenta em 1 a contagem de visualizalções da série quando for assistida por uma conta
-    {
-        this.contagemVisualizacao++;       
-    }
+    //MUDAR ISSO / Ctrl c v
+    public int getQuantidadeEpisodios() {
+		return this.quantidadeEpisodios;
+	}
 
-    public int getContagemVisualizacao() 
-    {
-        return this.contagemVisualizacao;
-    }
-    
-    public String getNome()
-    {
-        return this.nome;
-    }
+  
+	public void setQuantidadeEpisodios(int quantidadeEpisodios) {
+		this.quantidadeEpisodios = quantidadeEpisodios;
+	}
 
-    public String getIdioma()
-    {
-        return this.idioma;
-    }
 
-    public String getGenero()
-    {
-        return this.genero;
-    }
 
-    public boolean setIdioma(String idioma)
-    {
-        this.idioma = idioma;
-        return true;
-    }
-
-    public String getIdSerie()
-    {
-        return this.IdSerie;
-    }
 }
