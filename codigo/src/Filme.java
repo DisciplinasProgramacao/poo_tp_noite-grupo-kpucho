5de6534a4a54786573e93ff9fade2898f45b3b6a
Henrique Costa Marques <104331565+henriqueCMarques@users.noreply.github.com>
2023-05-17 11:33:09 -0300

    Update Filme.java

--- codigo/src/Filme.java
+++ codigo/src/Filme.java
@@ -1,25 +1,24 @@
 package src;
 
-public class Filme extends Midia
+public class Filme extends Midia 
 {
-    private int duracao;
-    private String descricao; // Não implementado
-    
-    public Filme(String IdFilme, String nome, String dataDeLancamento,String idioma, String genero, int duracao, int contagemVisualizacao)
-    {
-        super(IdFilme, nome, dataDeLancamento, idioma, genero, contagemVisualizacao);
-        this.duracao = duracao;
-    }
+  private int duracao;
+  private String descricao; // Não implementado
 
-    //MUDAR ISSO / Ctrl c v
-    public int getDuracao() 
-    {
-		return this.duracao;
-	}
-  
-	public void setDuracao(int duracao) 
-    {
-		this.duracao = duracao;
-	}
-    
+  public Filme(String IdFilme, String nome, String dataDeLancamento, String idioma, String genero, int duracao, int contagemVisualizacao) 
+  {
+    super(IdFilme, nome, dataDeLancamento, idioma, genero, contagemVisualizacao);
+    this.duracao = duracao;
+  }
+
+  public int getDuracao() 
+  {
+    return this.duracao;
+  }
+
+  public void setDuracao(int duracao) 
+  {
+    this.duracao = duracao;
+  }
+
 }
